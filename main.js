a90e2897a8f01af8e3ca7b66c59420824d97f795
Décio Silva <61292095+decito@users.noreply.github.com>
2026-08-22 23:16:02 -0300

    feat: traduzir e corrigir algumas strings (#21)
    - feat: alterar titulo do pacote para Português (Brasil)
    - feat: traduzir e corrigir algumas strings em equipment24
    
    - fix: corrigir simbolo de kilometros por hora
    
    - chore: incluir url do pacote no arquivo de modulo
    - chore: incluir nome dos contribuintes do projeto

--- main.js
+++ main.js
@@ -6,183 +6,201 @@ Hooks.once("babele.init", (babele) => {
   });
 
   babele.registerConverters({
-      "imperialToMetric": Converters.imperialToMetric(),
-      "tokens": Converters.tokens() 
+    imperialToMetric: Converters.imperialToMetric(),
+    tokens: Converters.tokens(),
   });
 });
 
 function convertMetricLength() {
-	return game.settings.get("dnd5e", "metricLengthUnits");
+  return game.settings.get("dnd5e", "metricLengthUnits");
 }
 
 function convertMetricWeight() {
-	return game.settings.get("dnd5e", "metricWeightUnits");
+  return game.settings.get("dnd5e", "metricWeightUnits");
 }
 
 function convertMetricVolume() {
-	return game.settings.get("dnd5e", "metricVolumeUnits");
+  return game.settings.get("dnd5e", "metricVolumeUnits");
 }
 
 export class Converters {
+  static imperialToMetric() {
+    return (value, translation, data) => Converters._imperialToMetric(value, translation, data);
+  }
 
-    static imperialToMetric() {
-        return (value, translation, data) => Converters._imperialToMetric(value, translation, data);
-    }
+  static _imperialToMetric(value, translation, data) {
+    const conversion = Converters.conversionInfo[value?.units ?? value?.template?.units ?? "ft"];
 
-    static _imperialToMetric(value, translation, data) {
-        const conversion = Converters.conversionInfo[value?.units ?? value?.template?.units ?? "ft"];
-        const converted = {};
-        if (conversion) {
-            if (value?.value) converted.value = conversion.converter(value.value);
-            if (value?.long) converted.long = conversion.converter(value.long);
-            if (value?.reach) converted.reach = conversion.converter(value.reach);
-            if (value?.distance) converted.distance = conversion.converter(value.distance);
-            if (value?.burrow) converted.burrow = conversion.converter(value.burrow);
-            if (value?.climb) converted.climb = conversion.converter(value.climb);
-            if (value?.swim) converted.swim = conversion.converter(value.swim);
-            if (value?.walk) converted.walk = conversion.converter(value.walk);
-            if (value?.fly) converted.fly = conversion.converter(value.fly);
-            if (value?.bright) converted.bright = conversion.converter(value.bright);
-            if (value?.dim) converted.dim = conversion.converter(value.dim);
-            if (value?.range) converted.range = conversion.converter(value.range);
-            if (value?.units) converted.units = conversion.units;
-            
-            if (value?.template) {
-                converted.template = {
-                    ...Object.fromEntries(
-                        ["size", "height", "width"]
-                            .map(k => [k, conversion.converter(value.template[k])])
-                    ),
-                    units: conversion.units
-                };
-            }
-            if (value?.ranges) {
-                converted.ranges = Object.fromEntries(
-                    ["darkvision", "blindsight", "tremorsense", "truesight"]
-                        .map(k => [k, conversion.converter(value.ranges[k])])
-                );
-            }
-            if (value?.paces) {
-                converted.paces = Object.fromEntries(
-                    ["air", "land", "water"].map(k => [k, conversion.converter(value.paces[k])])
-                );
-            }
-            if (value?.speeds) {
-                converted.speeds = Object.fromEntries(
-                    ["air", "land", "water"].map(k => [k, conversion.converter(value.speeds[k])])
-                );
-            }
-            
-            const detectionModeKeys = new Set(["lightPerception", "basicSight", "seeAll", "feelTremor", "blindsight", "senseInvisibility", "seeInvisibility", "senseAll"]);
-            if (Object.keys(value ?? {}).some(k => detectionModeKeys.has(k))) {
-                return Object.fromEntries(
-                    Object.entries(value).map(([k, mode]) => [
-                        k,
-                        mode?.range ? { ...mode, range: conversion.converter(mode.range) } : mode
-                    ])
-                );
-            }
-            if (value && typeof value === "object" && !Array.isArray(value) &&
-                Object.values(value).every(v => v && typeof v === "object" && "value" in v && Object.keys(v).length === 1)) {
-                return Object.fromEntries(
-                    Object.entries(value).map(([k, entry]) => [
-                        k, { value: conversion.converter(entry.value) }
-                    ])
-                );
-            }
-        }
-        
-        if (value?.affects?.special) converted.affects = { special: translation ?? value.affects.special };
-        if (value?.special) converted.special = translation ?? value.special;
+    const converted = {};
 
-        return Object.keys(converted).length ? foundry.utils.mergeObject(value, converted, { inplace: false }) : value;
-    }
+    if (conversion) {
+      // for (const key of ["bright", "burrow", "climb", "dim", "distance", "fly", "long", "paces", "range", "ranges", "reach", "speeds", "swim", "template", "units", "value", "walk"]) {
+      //   switch (key) {
+      //     case "bright":
+      //     case "burrow":
+      //     case "climb":
+      //     case "dim":
+      //     case "distance":
+      //     case "fly":
+      //     case "long":
+      //     case "range":
+      //     case "reach":
+      //     case "swim":
+      //     case "value":
+      //     case "walk":
+      //       if (value?.[key]) converted[key] = conversion.converter(value[key]);
+      //       break;
+      //     case "units":
+      //       if (value?.[key]) converted[key] = conversion.units;
+      //       break;
+      //     case "template":
+      //       if (value?.[key])
+      //         converted[key] = {
+      //           ...Object.fromEntries(["size", "height", "width"].map((k) => [k, conversion.converter(value.template[k])])),
+      //           units: conversion.units,
+      //         };
+      //       break;
+      //     case "ranges":
+      //       if (value?.[key]) converted[key] = Object.fromEntries(["darkvision", "blindsight", "tremorsense", "truesight"].map((k) => [k, conversion.converter(value.ranges[k])]));
+      //       break;
+      //     case "paces":
+      //       if (value?.[key]) converted[key] = Object.fromEntries(["air", "land", "water"].map((k) => [k, conversion.converter(value.paces[k])]));
+      //       break;
+      //     case "speeds":
+      //       if (value?.[key]) converted[key] = Object.fromEntries(["air", "land", "water"].map((k) => [k, conversion.converter(value.speeds[k])]));
+      //       break;
+      //     default:
+      //       break;
+      //   }
+      // }
+      if (value?.value) converted.value = conversion.converter(value.value);
+      if (value?.long) converted.long = conversion.converter(value.long);
+      if (value?.reach) converted.reach = conversion.converter(value.reach);
+      if (value?.distance) converted.distance = conversion.converter(value.distance);
+      if (value?.burrow) converted.burrow = conversion.converter(value.burrow);
+      if (value?.climb) converted.climb = conversion.converter(value.climb);
+      if (value?.swim) converted.swim = conversion.converter(value.swim);
+      if (value?.walk) converted.walk = conversion.converter(value.walk);
+      if (value?.fly) converted.fly = conversion.converter(value.fly);
+      if (value?.bright) converted.bright = conversion.converter(value.bright);
+      if (value?.dim) converted.dim = conversion.converter(value.dim);
+      if (value?.range) converted.range = conversion.converter(value.range);
+      if (value?.units) converted.units = conversion.units;
 
-    static get conversionInfo() {
-        return {
-            "ft": { converter: Converters.footsToMeters, units: convertMetricLength() ? "m" : "ft" },
-            "mi": { converter: Converters.milesToMeters, units: convertMetricLength() ? "km" : "mi" },
-            "mph": { converter: Converters.milesToMeters, units: convertMetricLength() ? "kph" : "mph" },
-            "lb": { converter: Converters.lbToKg, units: convertMetricWeight() ? "kg" : "lb" },
-            "cubicFoot": { converter: Converters.pcToL, units: convertMetricVolume() ? "liter" : "cubicFoot" }
+      if (value?.template) {
+        converted.template = {
+          ...Object.fromEntries(["size", "height", "width"].map((k) => [k, conversion.converter(value.template[k])])),
+          units: conversion.units,
         };
+      }
+      if (value?.ranges) {
+        converted.ranges = Object.fromEntries(["darkvision", "blindsight", "tremorsense", "truesight"].map((k) => [k, conversion.converter(value.ranges[k])]));
+      }
+      if (value?.paces) {
+        converted.paces = Object.fromEntries(["air", "land", "water"].map((k) => [k, conversion.converter(value.paces[k])]));
+      }
+      if (value?.speeds) {
+        converted.speeds = Object.fromEntries(["air", "land", "water"].map((k) => [k, conversion.converter(value.speeds[k])]));
+      }
+
+      const detectionModeKeys = new Set(["lightPerception", "basicSight", "seeAll", "feelTremor", "blindsight", "senseInvisibility", "seeInvisibility", "senseAll"]);
+
+      if (Object.keys(value ?? {}).some((k) => detectionModeKeys.has(k))) {
+        return Object.fromEntries(Object.entries(value).map(([k, mode]) => [k, mode?.range ? { ...mode, range: conversion.converter(mode.range) } : mode]));
+      }
+
+      if (value && typeof value === "object" && !Array.isArray(value) && Object.values(value).every((v) => v && typeof v === "object" && "value" in v && Object.keys(v).length === 1)) {
+        return Object.fromEntries(Object.entries(value).map(([k, entry]) => [k, { value: conversion.converter(entry.value) }]));
+      }
     }
 
-    static footsToMeters(ft) {
-        if (!convertMetricLength() || !ft || isNaN(parseInt(ft))) return ft;
-        return Converters.round(parseInt(ft) * 0.3);
-    }
+    if (value?.affects?.special) converted.affects = { special: translation ?? value.affects.special };
+    if (value?.special) converted.special = translation ?? value.special;
 
-    static milesToMeters(mi) {
-        if (!convertMetricLength() || !mi || isNaN(parseInt(mi))) return mi;
-        return Converters.round(parseInt(mi) * 1.5);
-    }
+    return Object.keys(converted).length ? foundry.utils.mergeObject(value, converted, { inplace: false }) : value;
+  }
 
-    static pcToL(pc) {
-        if (!convertMetricVolume() || !pc) return pc;
-        return Converters.round(parseInt(pc) * 28.317);
-    }
+  static get conversionInfo() {
+    return {
+      ft: { converter: Converters.footsToMeters, units: convertMetricLength() ? "m" : "ft" },
+      mi: { converter: Converters.milesToMeters, units: convertMetricLength() ? "km" : "mi" },
+      mph: { converter: Converters.milesToMeters, units: convertMetricLength() ? "km/h" : "mph" },
+      lb: { converter: Converters.lbToKg, units: convertMetricWeight() ? "kg" : "lb" },
+      cubicFoot: { converter: Converters.pcToL, units: convertMetricVolume() ? "L" : "cubicFoot" },
+    };
+  }
 
-    static lbToKg(lb) {
-        if (!convertMetricWeight() || !lb) return lb;
-        return parseInt(lb) / 2;
-    }
+  static footsToMeters(ft) {
+    if (!convertMetricLength() || !ft || isNaN(parseInt(ft))) return ft;
+    return Converters.round(parseInt(ft) * 0.3);
+  }
 
-    static round(num) {
-        return Math.round((num + Number.EPSILON) * 100) / 100;
-    }
+  static milesToMeters(mi) {
+    if (!convertMetricLength() || !mi || isNaN(parseInt(mi))) return mi;
+    return Converters.round(parseInt(mi) * 1.5);
+  }
 
-    static tokens() {
-        return (tokens, translations, data, tc, runtime = {}) => Converters._tokens(tokens, translations, data, tc, runtime);
-    }
+  static pcToL(pc) {
+    if (!convertMetricVolume() || !pc) return pc;
+    return Converters.round(parseInt(pc) * 28.317);
+  }
 
-    static _tokens(tokens, translations, data, tc, runtime = {}) {
-        tokens.map(token => {
-            return foundry.utils.mergeObject(token, {
-                light: Converters._imperialToMetric(token.light),
-                sight: Converters._imperialToMetric(token.sight)
-            });
-        });
+  static lbToKg(lb) {
+    if (!convertMetricWeight() || !lb) return lb;
+    return parseInt(lb) / 2;
+  }
 
-        if (!translations) return tokens;
+  static round(num) {
+    return Math.round((num + Number.EPSILON) * 100) / 100;
+  }
 
-        const documentMappings = 
-            runtime?.currentCompendium?.()?.documentMappings
-            ?? game.babele.documentMappings;
-        
-        const actorMapping = documentMappings.mappingFor("Actor");
+  static tokens() {
+    return (tokens, translations, data, tc, runtime = {}) => Converters._tokens(tokens, translations, data, tc, runtime);
+  }
 
-        const fakeCompendium = {
-            documentMappings,
-            translationMatchStrategies: () => []
-        };
+  static _tokens(tokens, translations, data, tc, runtime = {}) {
+    tokens.map((token) => {
+      return foundry.utils.mergeObject(token, {
+        light: Converters._imperialToMetric(token.light),
+        sight: Converters._imperialToMetric(token.sight),
+      });
+    });
 
-        const enrichedRuntime = typeof runtime?.child === "function"
-            ? runtime.child({ currentCompendium: fakeCompendium })
-            : { 
-                globalPacks: runtime?.globalPacks ?? new foundry.utils.Collection(),
-                localPacks: runtime?.localPacks ?? new foundry.utils.Collection(),
-                currentCompendium: fakeCompendium
-            };
+    if (!translations) return tokens;
 
-        return tokens.map(token => {
-            const translation = translations[token._id] || translations[token.name];
-            if (!translation) return token;
+    const documentMappings = runtime?.currentCompendium?.()?.documentMappings ?? game.babele.documentMappings;
 
-            const delta = token.delta ?? {};
-            let deltaTranslated = delta;
-            actorMapping.prepare(delta, translation, enrichedRuntime);
-            const payload = actorMapping.map(delta, translation, enrichedRuntime);
-            deltaTranslated = foundry.utils.mergeObject(
-                foundry.utils.deepClone(delta),
-                payload,
-                { inplace: false }
-            );
+    const actorMapping = documentMappings.mappingFor("Actor");
 
-            return foundry.utils.mergeObject(token, {
-                name: translation.name ?? token.name,
-                delta: deltaTranslated
-            }, { inplace: false });
-        });
-    }
+    const fakeCompendium = {
+      documentMappings,
+      translationMatchStrategies: () => [],
+    };
+
+    const enrichedRuntime =
+      typeof runtime?.child === "function"
+        ? runtime.child({ currentCompendium: fakeCompendium })
+        : {
+            globalPacks: runtime?.globalPacks ?? new foundry.utils.Collection(),
+            localPacks: runtime?.localPacks ?? new foundry.utils.Collection(),
+            currentCompendium: fakeCompendium,
+          };
+
+    return tokens.map((token) => {
+      const translation = translations[token._id] || translations[token.name];
+
+      if (!translation) return token;
+
+      const delta = token.delta ?? {};
+      let deltaTranslated = delta;
+
+      actorMapping.prepare(delta, translation, enrichedRuntime);
+
+      const payload = actorMapping.map(delta, translation, enrichedRuntime);
+
+      deltaTranslated = foundry.utils.mergeObject(foundry.utils.deepClone(delta), payload, { inplace: false });
+
+      return foundry.utils.mergeObject(token, { name: translation.name ?? token.name, delta: deltaTranslated }, { inplace: false });
+    });
+  }
 }
